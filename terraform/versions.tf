95c73f59501ba5037805b6eae7b5d263dbe93cb5
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2022-09-13 15:39:02 +0200

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,16 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.0"
+    }
+    databricks = {
+      source = "databrickslabs/databricks"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
